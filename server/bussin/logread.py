2ace0ad900da30bfd2a61d5764b9038bf8c78aec
cakleffman <cakleffman@gmail.com>
2022-11-29 21:23:12 -0500

    Added working traffic with dbc, updated logread and logwrite

--- server/bussin/logread.py
+++ server/bussin/logread.py
@@ -8,27 +8,31 @@ class read():
     def __init__(self):
         self.pkt=None
         self.cwd = os.getcwd()
-        self.db = cantools.db.load_file(self.cwd + "/CANBusReader/j1939_1.dbc")
-        self.bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate = 1000)
-        self.db_msg = self.db.get_message_by_name("Message")
+        self.db = cantools.db.load_file(self.cwd + "/j1939_1.dbc")
+        self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate = 25000)
+        self.db_msg = self.db.get_message_by_name("GFD")
         self.json = []
         self.decoded_json = []
 
+    def int_to_hex(self, nr):
+        h = format(int(nr), 'x')
+        return '0' + h if len(h) % 2 else h
 
     def recvDBC(self):
-
+        print("logread recvDBC entered")
         while True:
             msg = self.bus.recv(4)
             print("Message: ", msg)
             print("Channel Info", self.bus.channel_info, ".")
             if msg:
-                self.decoded - self.db.decode_message(msg.arbitration_id, msg.data)
+                msg_arbID = self.int_to_hex(msg.arbitration_id)
+                self.decoded = self.db.decode_message(msg.arbitration_id,msg.data)
                 print("Decoded Message:", self.db.decode_message(msg.arbitration_id, msg.data))
                 self.pkt =  msg
 
                 if self.pkt:
-                    self.writeJson()
-                    self.writeDecodedJson()
+                    self.EncodedJSON()
+                    self.DecodedJSON()
 
 
     def DecodedJSON(self, filename = "decoded_json.json"):
@@ -36,11 +40,10 @@ class read():
             self.decoded = str(self.decoded)
             values = self.decoded.split()
             self.decoded_json.append({
-                "Enable": values[1],
-                "AverageRadius": values[3],
-                "Temperature": values[5],
+                
+                
             })
-
+            print("len of values: ", len(values))
             json.dump(self.decoded_json, file, indent=2)
             print("Decoded JSON: ")
 
@@ -62,3 +65,5 @@ class read():
             })
             json.dump(self.json, file, indent=4)
             print("JSON created ...")
+    
+    
--- server/bussin/logwrite.py
+++ server/bussin/logwrite.py
@@ -5,9 +5,9 @@ import os
 class write():
     def __init__(self):
         self.cwd = os.getcwd()
-        self.db = cantools.db.load_file(self.cwd + "CANBusReader/j1939_1.dbc")
-        self.bus = can.interface.Bus(bustype='socketcan', channel = 'vcan0', bitrate = 1000)
-        self.db_msg = self.db.get_message_by_name("Message") 
+        self.db = cantools.db.load_file(self.cwd + "/j1939_1.dbc")
+        self.bus = can.interface.Bus(bustype='socketcan', channel = 'can0', bitrate = 1000)
+        self.db_msg = self.db.get_message_by_name("RBR") 
 
     def sendDBC(self):
         self.msgData = self.DBCmsg.encode({'Enable': 1, 'AverageRadius': 1, 'Temperature': 251})
